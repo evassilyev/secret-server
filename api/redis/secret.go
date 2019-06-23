bfb256c21e0476a6ac233275f0864435c23bfdcd
Evgen Vasilyev <evgen.vasilyev@gmail.com>
2019-06-23 06:55:02 +0200

    It works!

--- api/redis/secret.go
+++ api/redis/secret.go
@@ -3,9 +3,10 @@ package redis
 import (
 	"encoding/json"
 	"fmt"
+	"time"
+
 	"github.com/evassilyev/secret-server/api/core"
 	"github.com/go-redis/redis"
-	"time"
 )
 
 func NewSecretService(addr, pass string, db int) core.SecretService {
@@ -56,52 +57,57 @@ func (s *secretService) Save(secret string, eav, ea int) (core.Secret, error) {
 	return core.Secret(sr), err
 }
 
-// TODO add transaction
 func (s *secretService) Get(hash string) (res core.Secret, err error) {
 
-	counterKey := counterKey(hash)
-	err = s.redis.Decr(counterKey).Err()
-	if err != nil {
-		return
-	}
-	cmd := s.redis.Get(counterKey)
-	err = cmd.Err()
-	if err != nil {
-		return
-	}
-	remViews, err := cmd.Int()
-	if err != nil {
-		return
-	}
-
 	var sr secretRedisType
-	cmd = s.redis.Get(hash)
-	err = cmd.Err()
-	if err != nil {
-		return
-	}
-	err = cmd.Scan(&sr)
-	if err != nil {
-		return
-	}
-	sr.RemainingViews = remViews
+	counterKey := counterKey(hash)
 
-	if remViews <= 0 {
-		err = s.redis.Del(hash, counterKey).Err()
+	err = s.redis.Watch(func(tx *redis.Tx) error {
+		err := tx.Decr(counterKey).Err()
 		if err != nil {
-			return
+			return err
 		}
-	} else {
-		ttl := s.redis.TTL(hash)
-		err = ttl.Err()
+		cmd := tx.Get(counterKey)
+		err = cmd.Err()
 		if err != nil {
-			return
+			return err
 		}
-		err = s.redis.Set(hash, &sr, ttl.Val()).Err()
+		remViews, err := cmd.Int()
 		if err != nil {
-			return
+			return err
 		}
-	}
+
+		cmd = tx.Get(hash)
+		err = cmd.Err()
+		if err != nil {
+			return err
+		}
+		err = cmd.Scan(&sr)
+		if err != nil {
+			return err
+		}
+		sr.RemainingViews = remViews
+
+		if remViews <= 0 {
+			err = tx.Del(hash, counterKey).Err()
+			if err != nil {
+				return err
+			}
+		} else {
+			ttl := tx.TTL(hash)
+			err = ttl.Err()
+			if err != nil {
+				return err
+			}
+			err = tx.Set(hash, &sr, ttl.Val()).Err()
+			if err != nil {
+				return err
+			}
+		}
+
+		return err
+	}, counterKey, hash)
+
 	res = core.Secret(sr)
 	return
 }
